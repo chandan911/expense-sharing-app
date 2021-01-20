739805021e1e119c35fe62cf211f35c2ba368c11
Chandan Singh <chandan@Chandans-MacBook-Air.local>
2021-01-20 14:46:27 +0530

    Initial commit

---
+++ src/test/java/com/technogise/expensesharingapp/ExpenseSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technogise.expensesharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
