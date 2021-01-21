6bced6838962189836dd56723414bae8ecc9881c
shubham <shubhamsahu.15011998@gmail.com>
2021-01-21 22:24:25 +0530

    Enable auto filling of audit fields on all rows

--- src/main/java/com/technogise/expensesharingapp/ExpenseSharingAppApplication.java
+++ src/main/java/com/technogise/expensesharingapp/ExpenseSharingAppApplication.java
@@ -2,8 +2,10 @@ package com.technogise.expensesharingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ExpenseSharingAppApplication {
 
 	public static void main(String[] args) {
