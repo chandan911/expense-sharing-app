8d4cb03a6f5479a8c5660e1465fa15355d01e694
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-28 22:44:48 +0530

    Remove duplicate import packages due to merge

--- src/test/java/com/technogise/expensesharingapp/controllers/UserControllerTest.java
+++ src/test/java/com/technogise/expensesharingapp/controllers/UserControllerTest.java
@@ -21,23 +21,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.technogise.expensesharingapp.models.User;
 import com.technogise.expensesharingapp.services.UserService;
 import com.technogise.expensesharingapp.validators.Validator;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
