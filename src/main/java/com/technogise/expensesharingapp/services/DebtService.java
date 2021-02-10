7dd1f25658ae8afdc373611316d8842a10999e1f
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-02-10 11:49:27 +0530

    Add interface for methods 'updateDebtRepository' 'updateDebtProcess'

--- src/main/java/com/technogise/expensesharingapp/services/DebtService.java
+++ src/main/java/com/technogise/expensesharingapp/services/DebtService.java
@@ -1,6 +1,8 @@
 package com.technogise.expensesharingapp.services;
 
+import com.technogise.expensesharingapp.models.AddExpense;
 import com.technogise.expensesharingapp.models.Debt;
+import com.technogise.expensesharingapp.models.Expense;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,5 +12,8 @@ public interface DebtService {
 
   List<Debt> getAllDebtsByUserId(Long userId);
 
-  void updateDebtRepository(Long payerId, Long debtorId, Double debtAmount);
+  Boolean updateDebtRepository(Long payerId, Long debtorId, Double debtAmount);
+
+  Boolean updateDebtProcess(AddExpense addExpense);
+
 }
