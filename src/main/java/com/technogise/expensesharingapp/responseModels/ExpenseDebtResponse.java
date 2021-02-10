273717dc220a87b2dfc7ab4c512ead1e5cf84c6f
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-02-10 11:40:24 +0530

    Add methods 'hashcode' and 'equals' for testing purpose

--- src/main/java/com/technogise/expensesharingapp/responseModels/ExpenseDebtResponse.java
+++ src/main/java/com/technogise/expensesharingapp/responseModels/ExpenseDebtResponse.java
@@ -2,6 +2,7 @@ package com.technogise.expensesharingapp.responseModels;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 public class ExpenseDebtResponse implements Serializable {
 
@@ -31,5 +32,18 @@ public class ExpenseDebtResponse implements Serializable {
 
     public void setDebts(List<DebtResponse> debts) {
         this.debts = debts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExpenseDebtResponse)) return false;
+        ExpenseDebtResponse that = (ExpenseDebtResponse) o;
+        return expenses.equals(that.expenses) && debts.equals(that.debts) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expenses, debts);
     }
 }
